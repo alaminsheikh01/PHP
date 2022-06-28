cddbf2d65692f68fc87545cfc16067021a54c7f9
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-06-28 23:54:03 +0600

    bug fixed

--- logout.php
+++ logout.php
@@ -3,6 +3,13 @@
 session_start();
 session_destroy();
 echo "You successfully Logout <br>";
+if(isset($_COOKIE['email']) and isset($_COOKIE['pass'])) {
+    $email = $_COOKIE['email'];
+    $pass = $_COOKIE['pass'];
+    setcookie('email', $email, time()-1);
+    setcookie('pass', $pass, time()-1); 
+  }
+
 echo "Go to <a href='index.php'> Login</a> page "
 
 ?>

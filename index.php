e46c44f098f691d69bd753e493e146c8ac88cf33
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-06-28 12:17:55 +0600

    include differetn pages

--- index.php
+++ index.php
@@ -1,27 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-  </head>
-  <body>
-    <?php
-
-    $array = array("Alamin", "sheikh", "muhammad");
-
-    foreach($array as $loopdata) {
-      echo "my name is ".$loopdata."<br/>";
-    }
-
-    function discount($x){
-      $val = $x * .50;
-      echo "Here is 50% of what you wrote: " .$val;
-    }
-    discount(100)
-
-
-    ?>
+<?php
+ include 'header.php'
+?>
+  <header>
+    <nav>
+      <ul>
+        <li><a href="">nav1</a></li>
+        <li><a href="">nav2</a></li>
+        <li><a href="">nav3</a></li>
+        <li><a href="">nav4</a></li>
+      </ul>
+    </nav>
+  </header>
+    <?php?>
   </body>
 </html>
